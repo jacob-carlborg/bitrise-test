fe7c5a3d19625e6d03e245939b7580085a29f82c
Jacob Carlborg <doob@me.com>
2020-01-24 19:18:29 +0100

    Initial commit

---
+++ bitrise-testTests/bitrise_testTests.m
@@ -0,0 +1,21 @@
+#import <XCTest/XCTest.h>
+
+@interface bitrise_testTests : XCTestCase
+
+@end
+
+@implementation bitrise_testTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    printf("***************************** testExample\n");
+}
+
+@end
