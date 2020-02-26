3ed18dfbeb74dac0c15c8a1166ac411e43dff0a6
Jacob Carlborg <doob@me.com>
2020-02-26 20:40:33 +0100

    Print some output

--- bitrise-testTests/bitrise_testTests.m
+++ bitrise-testTests/bitrise_testTests.m
@@ -16,8 +16,10 @@
 
 - (void)testExample {
 #ifdef __arm64__
+    printf("************************* ARM64\n");
     XCTAssert(true, "ARM64");
 #elif __x86_64__
+    printf("************************* x86-64\n");
     XCTAssert(false, "x86-64");
 #endif
 }
