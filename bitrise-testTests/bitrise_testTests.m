c52cbf169f5659c2810f24af7554436f06252adc
Jacob Carlborg <doob@me.com>
2020-02-26 20:46:30 +0100

    Fail on ARM64 as well

--- bitrise-testTests/bitrise_testTests.m
+++ bitrise-testTests/bitrise_testTests.m
@@ -17,7 +17,7 @@
 - (void)testExample {
 #ifdef __arm64__
     printf("************************* ARM64\n");
-    XCTAssert(true, "ARM64");
+    XCTAssert(false, "ARM64");
 #elif __x86_64__
     printf("************************* x86-64\n");
     XCTAssert(false, "x86-64");
